5c63cac5f437d673486d64e4cfe6963cbfa0a07e
Madhukar Moogala <madhukar.moogala@autodesk.com>
2022-07-02 04:13:57 +0530

    first push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React  from "react";
+import {createRoot} from "react-dom/client"
+function Main(){
+    return (<><p>That was a nice Cake!</p><button type ="button" className="btn btn-primary">Hello</button></>);
+}
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render([<Main/>,page]);
