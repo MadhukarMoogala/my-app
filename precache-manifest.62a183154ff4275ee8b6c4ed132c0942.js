b842359b189da875417fd04cf5dbed285624834b
Madhukar Moogala <madhukar.moogala@autodesk.com>
2022-07-02 04:32:41 +0530

    Updates

---
+++ precache-manifest.62a183154ff4275ee8b6c4ed132c0942.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "151822fba31e3a6e66d2",
+    "url": "/my-app/static/js/main.151822fb.chunk.js"
+  },
+  {
+    "revision": "ea1774b402de21949992",
+    "url": "/my-app/static/js/1.ea1774b4.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "47f2d95a5ba02c32c27f9dd24366c20c",
+    "url": "/my-app/index.html"
+  }
+];
